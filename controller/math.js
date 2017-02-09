410b6fcd2b8e52f812f930a98cf05a6c4415f781
Irsan Sebastian <irsansebastianc4@gmail.com>
2017-02-09 11:20:58 +0700

    controller

---
+++ controller/math.js
@@ -0,0 +1,14 @@
+module.exports = {
+  pertambahan: function (num_1, num_2) {
+    return num_1 + num_2
+  },
+  perkurangan: function (num_1, num_2) {
+    return num_1 - num_2
+  },
+  pembagian: function (num_1, num_2) {
+    return num_1 / num_2
+  },
+  perkalian: function (num_1, num_2) {
+    return num_1 * num_2
+  }
+}
