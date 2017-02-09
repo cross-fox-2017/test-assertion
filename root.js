b2307b83171b0594b5452353a3208429a131ea3a
yonisetiawan <yonisetiawan1990@gmail.com>
2017-02-09 14:54:30 +0700

    Create Function Root

---
+++ root.js
@@ -0,0 +1,15 @@
+
+// const assert = require('assert');
+
+var Root = function(a, b) {
+  if(typeof(a)=="number" && typeof(b)=="number"){
+    return Math.pow(a,b)
+  }else{
+    return "cek input parameter"
+  }
+}
+
+module.exports = Root
+
+// assert.equal(Root(5,5),3125)
+// assert.equal(Root(2),false)
