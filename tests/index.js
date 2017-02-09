ce7081f86dd8c18b67600b8040f7dd3c0ada2839
qblol <iqbal.0809@gmail.com>
2017-02-09 11:20:11 +0700

    ngerapihin

--- tests/index.js
+++ tests/index.js
@@ -10,11 +10,9 @@ assert.equal(math.substract(3,2),'1')
 assert.notEqual(math.substract(3,2),'6')
 assert.strictEqual(math.substract(3,2),1)
 
-
 assert.equal(math.multiply(3,2),'6')
 assert.notEqual(math.multiply(3,2),'7')
 assert.strictEqual(math.multiply(3,2),6)
-
 
 assert.equal(math.divide(3,2),'1.5')
 assert.notEqual(math.divide(3,2),'6')
@@ -23,7 +21,6 @@ assert.strictEqual(math.divide(3,2),1.5)
 assert.equal(math.exponent(3,2),'9')
 assert.notEqual(math.exponent(3,2),'6')
 assert.strictEqual(math.exponent(3,2),9)
-
 
 assert.equal(math.root(3,2),'1.7320508075688772')
 assert.notEqual(math.root(3,2),'6')
