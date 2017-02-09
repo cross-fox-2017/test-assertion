48f07336e9f8db25fa76bf5608755564e2a935c8
yonisetiawan <yonisetiawan1990@gmail.com>
2017-02-09 14:49:13 +0700

    Create Function Add

---
+++ add.js
@@ -0,0 +1,15 @@
+
+// const assert = require('assert');
+
+var Add = function(a, b) {
+  if(typeof(a)=="number" && typeof(b)=="number"){
+    return a+b
+  }else{
+    return "cek input parameter"
+  }
+}
+
+module.exports = Add
+
+// assert.equal(Add(1,1),2)
+// assert.equal(Add(2),false)
