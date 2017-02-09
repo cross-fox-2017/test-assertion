b7077c0cf1fd3ce09575a15f03edd784a27ae0dd
Syanmil <syanmil@yahoo.com>
2017-02-09 10:29:01 +0700

    skeleton function

---
+++ test-assertion.js
@@ -0,0 +1,21 @@
+'use strict'
+var assert = require('assert')
+
+function penambahan(angka) {
+
+}
+function pengurangan() {
+
+}
+function perkalian() {
+
+}
+function pembagian() {
+
+}
+function pangkat() {
+
+}
+function akar() {
+
+}
