b01c0b606664d8494d3a82a40e4cf629e3afb58f
timocodex <timothy.og.siahaan@gmail.com>
2017-02-09 12:08:51 +0700

    done negative test case

--- exponent.js
+++ exponent.js
@@ -1,4 +1,12 @@
 function pangkat(a,b){
-  return Math.pow(a,b)
+  if(isNaN(a)&&isNaN(b)){
+    return "isi parameternya"
+  }
+  if(isNaN(a)||isNaN(b)){
+    return "parmeternya kurang 1"
+  }
+  else{
+    return Math.pow(a,b)
+  }
 }
 module.exports=pangkat
