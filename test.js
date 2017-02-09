f963f0df4e621139f4c5acde249bb746a7911b0a
Hacktiv8 <hacktiv8-11@Hacktiv8-11.local>
2017-02-09 10:44:35 +0700

    beres not equal

--- test.js
+++ test.js
@@ -20,8 +20,19 @@ function akardari(num){
 }
 
 assert.equal(pertambahan(1,5), 6);
+assert.notEqual(pertambahan(1,5), 0);
+
 assert.equal(pengurangan(10,5), 5);
+assert.notEqual(pengurangan(10,5), 0);
+
 assert.equal(perkalian(1,5), 5);
+assert.notEqual(perkalian(1,5), 0);
+
 assert.equal(pembagian(64,2), 32);
+assert.notEqual(pembagian(1,5), 0);
+
 assert.equal(pangkatdua(5), 25);
+assert.notEqual(pangkatdua(10), 25);
+
 assert.equal(akardari(9), 3);
+assert.notEqual(akardari(12), 3);
