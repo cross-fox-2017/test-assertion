2594da7556b7a1f2efe679d137665eb81e13465b
yonisetiawan <yonisetiawan1990@gmail.com>
2017-02-09 14:49:39 +0700

    Create function divide

---
+++ divide.js
@@ -0,0 +1,15 @@
+
+// const assert = require('assert');
+
+var Divide = function(a, b) {
+  if(typeof(a)=="number" && typeof(b)=="number"){
+    return a-b
+  }else{
+    return "cek input parameter"
+  }
+}
+
+module.exports = Divide
+
+// assert.equal(Divide(1,1),0)
+// assert.equal(Divide(2),false)
