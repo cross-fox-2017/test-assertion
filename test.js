fe5756e54acff0b460abbf5310fcb795883e155f
Hacktiv8 <hacktiv8-11@Hacktiv8-11.local>
2017-02-09 10:54:00 +0700

    ganti modular

--- test.js
+++ test.js
@@ -1,38 +1,21 @@
+const fungsi = require('./fungsi')
 const assert = require('assert');
 
-function pertambahan(a,b){
-  return a+b
-}
-function pengurangan(a,b){
-  return a-b
-}
-function perkalian(a,b){
-  return a*b
-}
-function pembagian(a,b){
-  return a/b
-}
-function pangkatdua(num){
-  return Math.pow(num,2)
-}
-function akardari(num){
-  return Math.sqrt(num)
-}
+assert.equal(fungsi.pertambahan(1,5), 6);
+assert.notEqual(fungsi.pertambahan(1,5), 0);
+assert.strictEqual(fungsi.pertambahan(1,5), 6);
 
-assert.equal(pertambahan(1,5), 6);
-assert.notEqual(pertambahan(1,5), 0);
+assert.equal(fungsi.pengurangan(10,5), 5);
+assert.notEqual(fungsi.pengurangan(10,5), 0);
 
-assert.equal(pengurangan(10,5), 5);
-assert.notEqual(pengurangan(10,5), 0);
+assert.equal(fungsi.perkalian(1,5), 5);
+assert.notEqual(fungsi.perkalian(1,5), 0);
 
-assert.equal(perkalian(1,5), 5);
-assert.notEqual(perkalian(1,5), 0);
+assert.equal(fungsi.pembagian(64,2), 32);
+assert.notEqual(fungsi.pembagian(1,5), 0);
 
-assert.equal(pembagian(64,2), 32);
-assert.notEqual(pembagian(1,5), 0);
+assert.equal(fungsi.pangkatdua(5), 25);
+assert.notEqual(fungsi.pangkatdua(10), 25);
 
-assert.equal(pangkatdua(5), 25);
-assert.notEqual(pangkatdua(10), 25);
-
-assert.equal(akardari(9), 3);
-assert.notEqual(akardari(12), 3);
+assert.equal(fungsi.akardari(9), 3);
+assert.notEqual(fungsi.akardari(12), 3);
