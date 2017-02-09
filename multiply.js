c51b70b539910073f4cb6ce75a2d4967079a3a1f
yonisetiawan <yonisetiawan1990@gmail.com>
2017-02-09 14:51:20 +0700

    Create Function Multiply

---
+++ multiply.js
@@ -0,0 +1,15 @@
+
+// const assert = require('assert');
+
+var Multiply = function(a, b) {
+  if(typeof(a)=="number" && typeof(b)=="number"){
+    return a*b
+  }else{
+    return "cek input parameter"
+  }
+}
+
+module.exports = Multiply
+
+// assert.equal(Multiply(2,2),4)
+// assert.equal(Multiply(2),false)
