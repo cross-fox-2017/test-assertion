39532efbd456e8112a61c7b55ae1662b7bbf758c
yonisetiawan <yonisetiawan1990@gmail.com>
2017-02-09 14:58:03 +0700

    Create Function Substract

---
+++ substract.js
@@ -0,0 +1,15 @@
+
+const assert = require('assert');
+
+var Substract = function(a) {
+  if(typeof(a)=="number"){
+    return Math.sqrt(9)
+  }else{
+    return "cek input parameter"
+  }
+}
+
+module.exports = Substract
+
+assert.equal(Substract(9),3)
+assert.equal(Substract(),false)
