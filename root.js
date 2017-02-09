d3dd1dd3569310c8ae38bac486482668272deea9
Didit Suryadi <email": null>
2017-02-09 11:30:52 +0700

    create-modul

---
+++ root.js
@@ -0,0 +1,5 @@
+function (a){
+  return Math.sqrt(a)
+}
+
+module.exports = root ;
---
+++ tests/index.js
@@ -0,0 +1,26 @@
+var assert = require('assert')
+var math = require('./tests/index.js')
+
+assert.equal();
+assert.notEqual();
+assert.strictEqual();
+
+assert.equal();
+assert.notEqual();
+assert.strictEqual();
+
+assert.equal();
+assert.notEqual();
+assert.strictEqual();
+
+assert.equal();
+assert.notEqual();
+assert.strictEqual();
+
+assert.equal();
+assert.notEqual();
+assert.strictEqual();
+
+assert.equal();
+assert.notEqual();
+assert.strictEqual();
